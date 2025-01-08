621c284febe2a87380484d7cc174ccb413910877
Nayan <nayanmitra844@gmail.com>
2025-01-08 17:34:44 +0530

    Commiting 8 jan portfolio

---
+++ nyn.js
@@ -0,0 +1,9 @@
+import 'boxicons'
+
+const menu = document.querySelector("#menu");
+const nav = document.querySelector(".links");
+
+menu.onclick =()=> {
+  menu.classList.toggle('bx-x');
+  nav.classList.toggle('active');
+}
